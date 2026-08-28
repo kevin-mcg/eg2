d0075889dc24fd8b5dfd81337bbd6031809a219c
kevin-mcg <kevin.m.mcguinness@gmail.com>
2026-08-28 14:41:58 +0200

    Editing & updating EGs

--- app/controllers/escape_games_controller.rb
+++ app/controllers/escape_games_controller.rb
@@ -6,4 +6,16 @@ class EscapeGamesController < ApplicationController
   def show
     @escape_game = EscapeGame.find(params[:id])
   end
+
+  def edit
+    @escape_game = EscapeGame.find(params[:id])
+  end
+
+  def update
+    @escape_game = EscapeGame.find(params[:id])
+    escape_game_params = params.require(:escape_game).
+                          permit(:title, :description, :difficulty)
+    @escape_game.update(escape_game_params)
+    redirect_to @escape_game
+  end
 end
