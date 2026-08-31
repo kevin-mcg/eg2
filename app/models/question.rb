f5693640391d6985998d0b444f5f0c66a6861716
kevin-mcg <kevin.m.mcguinness@gmail.com>
2026-08-31 16:17:07 +0200

    Validations for the escape_game and question models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :escape_game
+
+  validates :question, presence: true, length: { minimum: 10 }
+  validates :answer, presence: true, length: { minimum: 1, allow_blank: true }
 end
