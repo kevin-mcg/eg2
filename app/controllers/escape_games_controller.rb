d4f5dc51dc73f5ee7e4ef1afa4aa1406c4385f52
kevin-mcg <kevin.m.mcguinness@gmail.com>
2026-09-05 09:16:53 +0200

    Checking all answers WIP avec R

--- app/controllers/escape_games_controller.rb
+++ app/controllers/escape_games_controller.rb
@@ -34,10 +34,84 @@ class EscapeGamesController < ApplicationController
     redirect_to @escape_game, status: :see_other
   end
 
+  def check_answers
+    @escape_game = EscapeGame.find(params[:id])
+    submitted = params[:answers] || {}
+
+    puts "The submitted answers are:"
+    puts submitted
+
+    @results = @escape_game.questions.index_with do |q|
+      puts "So, submitted[q.id.to_s]) is:"
+      puts submitted[q.id.to_s]
+      q.correct?(submitted[q.id.to_s])
+    end
+
+    # @results = @escape_game.questions.index_with do |q| q.correct?(submitted[q.id.to_s]) end
+
+    puts "The @results are:"
+    puts @results
+    puts "@results.first"
+    puts @results.first
+    # puts "@results.first class?"
+    # puts @results.first.class
+    # Array
+
+    # @results
+    # => 
+    # {#<Question:0x000072b93c00ac90
+    #   id: 12,
+    #   question: "Well, did you?",
+    #   answer: "yes",
+    #   escape_game_id: 3,
+    #   created_at: "2026-09-02 13:40:04.347530000 +0000",
+    #   updated_at: "2026-09-02 13:40:04.347530000 +0000">=>true}
+    # eg2(dev)> @results.first
+    # => 
+    # [#<Question:0x000072b93c00ac90
+    #   id: 12,
+    #   question: "Well, did you?",
+    #   answer: "yes",
+    #   escape_game_id: 3,
+    #   created_at: "2026-09-02 13:40:04.347530000 +0000",
+    #   updated_at: "2026-09-02 13:40:04.347530000 +0000">,
+    # true]
+    # eg2(dev)> @results.first.last
+    # => true
+
+    
+
+
+    if @results.first.last == true
+      puts "KMcG - correct answer here"
+      redirect_to @escape_game,
+        notice: 'Yay, your answer(s) are correct!'
+    else
+      puts "KMcG - incorrect answer here"
+      redirect_to @escape_game,
+        notice: 'At least one of your answers is incorrect. Try again.'
+    end
+
+    # respond_to do |format|
+    #   puts "The format is:"
+    #   puts format
+    #   puts format.inspect
+    #   puts "KMcG1"
+    #   format.turbo_stream
+    #   puts "KMcG2"
+    #   format.html { redirect_to @escape_game }
+    # end
+  end
+
   private
 
   def escape_game_params
     params.require(:escape_game).
             permit(:title, :description, :difficulty)
+  end
+
+  def build_feedback(results)
+    correct_count = results.values.count(true)
+    "#{correct_count}/#{results.size} bonnes réponses"
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,10 @@ class Question < ApplicationRecord
 
   validates :question, presence: true, length: { minimum: 10 }
   validates :answer, presence: true, length: { minimum: 1, allow_blank: true }
+
+  def correct?(given_answer)
+    puts "The given_answer is:"
+    puts given_answer
+    answer.to_s.strip.downcase == given_answer.to_s.strip.downcase
+  end
 end
