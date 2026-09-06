94ac6964dbc91007963db2febabcb71c221b77b0
kevin-mcg <kevin.m.mcguinness@gmail.com>
2026-09-06 10:32:46 +0200

    WIP editing and updating questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,6 +14,26 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def edit
+    @escape_game = EscapeGame.find(params[:escape_game_id])
+    @questions = @escape_game.questions
+  end
+
+  def update
+    puts "Are we here at all?"
+    @escape_game = EscapeGame.find(params[:escape_game_id])
+    puts "The first question is:"
+    puts @escape_game.questions.first
+    @question = @escape_game.questions.first
+    if @question.update(question_params)
+      puts "Updated!!!"
+      redirect_to escape_game_path(@escape_game)
+    else
+      puts "Oh noooo!!!"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def question_params
