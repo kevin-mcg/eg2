7f0d5e07ad72b346af9c153b259f4873d892dd8c
kevin-mcg <kevin.m.mcguinness@gmail.com>
2026-09-10 14:51:15 +0200

    Sign out can be done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    reset_session
+    redirect_to escape_games_path, status: :see_other
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def current_user
+    # memoization - to avoid additional cache calls to find the user...
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
