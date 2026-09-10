770f587a0856629436d419b22a2040dbb3e250ba
kevin-mcg <kevin.m.mcguinness@gmail.com>
2026-09-10 20:10:36 +0200

    admin column added plus admin auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :require_signin, except: [:new, :create]
-  before_action :require_correct_user, only: [:edit, :update, :destroy]
+  before_action :require_correct_user, only: [:edit, :update]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.all
@@ -38,9 +39,10 @@ class UsersController < ApplicationController
 
 
   def destroy
+    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
-    redirect_to @user, status: :see_other,
+    redirect_to root_url, status: :see_other,
       alert: "Account successfully deleted!"
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_09_09_123018) do
+ActiveRecord::Schema[7.2].define(version: 2026_09_10_174814) do
   create_table "escape_games", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_09_09_123018) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "questions", "escape_games"
